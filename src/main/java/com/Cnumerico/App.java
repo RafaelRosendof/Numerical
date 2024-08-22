9e11885e9f41d21825e814a7994eac0592518a83
Rafael <rafael.19.rosendo@gmail.com>
2024-08-22 13:44:59 -0300

    hello world javaFX

--- src/main/java/com/Cnumerico/App.java
+++ src/main/java/com/Cnumerico/App.java
@@ -1,16 +1,43 @@
 package com.Cnumerico;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+
+public class App extends Application 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
 
-        System.out.println("TESTANDO O COMPILER");
+    @Override
+    //hello world em javaFX
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Hello Calculo numérico!");
+        Button btn = new Button();
+        btn.setText("Fala Figas blz?");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+                
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Fala Figas blz?");
+                }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root , 400 , 500));
+        primaryStage.show();
     }
 }
 
